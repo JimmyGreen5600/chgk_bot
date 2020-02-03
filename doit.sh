9e67509fd5c3428d3342f2227430c5be10b4a26d
Your Name <bestsexevar@gmail.com>
2020-02-03 17:54:45 +0300

    script sending 24 questions at once

--- doit.sh
+++ doit.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-chgk_bot/parse.py > quest.txt
+./parse.py > quest.txt
 file="quest.txt"
 IFS=$'\n'
 for var in $(cat $file)
 do
-" $var"
-chgk_bot/script.sh "$(cat $file)"
+./script.sh "$(cat $file)"
 done
